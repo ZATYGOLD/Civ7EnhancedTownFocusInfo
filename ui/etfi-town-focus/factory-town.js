045d491370ab6f483355f5129e3a251e3ffc276e
ZATYGOLD <30324251+ZATYGOLD@users.noreply.github.com>
2026-05-22 22:12:52 +0800

    Forgotten changes

--- ui/etfi-town-focus/factory-town.js
+++ ui/etfi-town-focus/factory-town.js
@@ -1,8 +1,10 @@
 // File Path: ui/etfi-town-focus/factorytown.js
 
 // Factory Town: 
-// +5 Healing to Units and +25 Health to Fortifications. +1 Gold on Fortified Districts. 
-// Can purchase Military Buildings and Fortifications.    
+// +100% Gold towards purchasing a Factory, Port, or Rail Station in this town.
+// +1 Resource Slot. +5 Trade Range. 
+// Can Purchase additional Gold buildings in this Town. 
+// Must have an improved Factory Resrouce in this settlement.  
 import { ETFI_YIELDS } from "../../etfi-utilities.js";
 
 export default class FactoryTownDetails {
